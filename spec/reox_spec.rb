38bd4416f1f4a38115a47eeafdb5141bfe74d52b
Jatin Dhankhar <dhankhar.jatin@gmail.com>
2019-05-04 16:43:53 +0530

    Add specs for bang methods

--- spec/reox_spec.rb
+++ spec/reox_spec.rb
@@ -1,18 +1,27 @@
 require 'reox'
 
 RSpec.describe Reox do
+  def verify_recording(msg, verbose = false)
+    file_content = File.read('output.log')
+    if verbose
+      expect(file_content.strip).to include(msg)
+      expect(file_content.strip).to_not eq(msg)
+    else
+      expect(file_content.strip).to eq(msg)
+    end
+  end
+
   before(:each) do
     File.delete('output.log') if File.exist?('output.log')
   end
 
   describe '#record' do
-    it 'records the content in non-verbose mode inside the block if no option is provided' do
+    it 'records the content in non-verbose mode inside the block by default' do
       msg = 'It will be recorded'
       Reox.record do
         puts msg
       end
-      file_content = File.read('output.log')
-      expect(file_content.strip).to eq(msg)
+      verify_recording(msg)
     end
 
     it 'records the content verbosely inside the block if verbose option is provided' do
@@ -20,9 +29,25 @@ RSpec.describe Reox do
       Reox.record(verbose: true) do
         puts msg
       end
-      file_content = File.read('output.log')
-      expect(file_content.strip).to include(msg)
-      expect(file_content.strip).to_not eq(msg)
+      verify_recording(msg, true)
+    end
+  end
+
+  describe '#record!' do
+    it 'records the content in non-verbose mode inside the block by default' do
+      msg = 'It will be recorded'
+      Reox.record!
+      puts msg
+      Reox.stop!
+      verify_recording(msg)
+    end
+
+    it 'records the content verbosely inside the block if verbose option is provided' do
+      msg = 'It will be recorded verbosely'
+      Reox.record!(verbose: true)
+      puts msg
+      Reox.stop!
+      verify_recording(msg, true)
     end
   end
 end
