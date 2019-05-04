7fae610be888634897528b335346a2ff374d696d
Jatin Dhankhar <dhankhar.jatin@gmail.com>
2019-05-04 16:00:19 +0530

    Add specs for verbose mode

--- spec/reox_spec.rb
+++ spec/reox_spec.rb
@@ -6,11 +6,23 @@ RSpec.describe Reox do
   end
 
   describe '#record' do
-    it 'records the content inside the block' do
+    it 'records the content in non-verbose mode inside the block if no option is provided' do
+      msg = 'It will be recorded'
       Reox.record do
-        puts 'It will be recorded'
+        puts msg
       end
-      expect(File.read('output.log').strip).to eq('It will be recorded')
+      file_content = File.read('output.log')
+      expect(file_content.strip).to eq(msg)
+    end
+
+    it 'records the content verbosely inside the block if verbose option is provided' do
+      msg = 'It will be recorded verbosely'
+      Reox.record(verbose: true) do
+        puts msg
+      end
+      file_content = File.read('output.log')
+      expect(file_content.strip).to include(msg)
+      expect(file_content.strip).to_not eq(msg)
     end
   end
 end
